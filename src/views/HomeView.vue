7ea00347f4cf6a844cc76c674d490df2191bb9cf
yofandirikiwinata <yofandirikiwinata24@gmail.com>
2024-08-24 13:32:56 +0700

    add: Menggunakan Kondisi Dalam Komponen Transition

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,14 +6,29 @@ const showText = ref(false);
 
 <template>
   <main>
-    <Transition name="fade">
-      <h1 v-if="showText">hello world</h1>
-    </Transition>
-    <button @click="showText = !showText">toggle</button>
+    <div class="container">
+      <Transition name="fade">
+        <h1 v-if="showText">hello world</h1>
+        <h1 v-else>Good Bye</h1>
+      </Transition>
+      <button @click="showText = !showText">toggle</button>
+    </div>
   </main>
 </template>
 
 <style scoped>
+.container {
+  position: relative;
+}
+
+h1 {
+  position: absolute;
+}
+
+button {
+  margin-top: 100px;
+}
+
 .fade-enter-from {
   opacity: 0;
 }
